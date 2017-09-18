0663677930b0a348ba7ebd663199575453c64622
Trevor <trevorhoward2@hotmail.com>
2017-09-17 22:01:50 -0600

    Fix routing error, change how it works link in footer

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,5 @@ Rails.application.routes.draw do
   mount Spree::Core::Engine, at: '/'
           # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get 'howitworks', to: 'spree/home#how_it_works', as: :home
+  get 'howitworks', to: 'spree/home#how_it_works'
 end
