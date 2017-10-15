73fd6545fb7f012ceb8d00ff151aa8776fb606de
Trevor <trevorhoward2@hotmail.com>
2017-10-15 14:08:01 -0600

    Added a black logo for default so hopefully when linking to the site it will display correctly

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -13,7 +13,7 @@ Spree.config do |config|
   # Example:
   # Uncomment to stop tracking inventory levels in the application
   # config.track_inventory_levels = false
-  config.logo = "missionarybox.png"
+  config.logo = "logo.png"
   config.shipping_instructions = true
 end
 
